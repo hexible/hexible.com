381ea1dbc591c1b888f883c1b9d9a930062d1629
heydeepakjangra <heydeepakjangra@gmail.com>
2025-01-24 23:03:26 +0530

    Reorganize exports in UI component index file

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,8 +1,6 @@
-export * from "./custom";
 export * from "./button";
 export * from "./sheet";
 export * from "./radio-group";
-export * from "./input"
-export * from "./textarea"
-export * from "./label"
-
+export * from "./input";
+export * from "./textarea";
+export * from "./label";
