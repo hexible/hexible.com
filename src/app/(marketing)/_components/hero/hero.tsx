0900f0b1b102c8ef18f2d74a9fc8592ca5c8a5d1
heydeepakjangra <heydeepakjangra@gmail.com>
2025-02-16 20:07:20 +0530

    improved hero ui

--- src/app/(marketing)/_components/hero/hero.tsx
+++ src/app/(marketing)/_components/hero/hero.tsx
@@ -7,26 +7,37 @@ import Link from "next/link";
 export const Hero = ({ ...rest }: React.ComponentProps<"main">) => {
   return (
     <main
-      className="justify-between items-center grid grid-cols-1 md:grid-cols-2 mt-10 md:mt-16 h-full"
+      className="justify-between items-center grid grid-cols-1 lg:grid-cols-3 mt-16 md:mt-24 h-full"
       {...rest}
     >
-      <div className="order-2 md:order-1">
-        <h1 className="font-extrabold text-3xl md:text-4xl leading-tight">
+      <div className="order-2 md:order-1 lg:col-span-2">
+        <h1 className="font-extrabold text-3xl md:text-6xl leading-tight">
           {AppData.taglinePart1}, <br /> {AppData.taglinePart2}
         </h1>
         <h2 className="my-4">{AppData.longDescription}</h2>
-        <Link
-          href={AppData.consultation.link}
-          className={cn(buttonVariants())}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {AppData.consultation.title}
-        </Link>
+        <div className="flex flex-wrap justify-start gap-4 mt-6">
+          <Link
+            href={AppData.requestQuote.link}
+            className={cn(
+              buttonVariants({ variant: "secondary" }),
+              "w-full md:w-auto px-6"
+            )}
+          >
+            {AppData.requestQuote.title}
+          </Link>
+          <Link
+            href={AppData.consultation.link}
+            className={cn(buttonVariants(), "w-full md:w-auto px-6")}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {AppData.consultation.title}
+          </Link>
+        </div>
       </div>
 
-      <div className="justify-center items-center order-1 md:order-2">
-        <HeroImg className="w-full h-72 md:h-80" />
+      <div className="hidden lg:block justify-center items-center order-1 md:order-2 lg:col-span-1">
+        <HeroImg className="w-full h-72" />
       </div>
     </main>
   );
